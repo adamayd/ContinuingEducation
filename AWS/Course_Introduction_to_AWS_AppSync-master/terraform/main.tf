4ad8606d8dd46528a02e14a68429a4ec4ce7bbb3
Adam Ayd <adam.ayd@gmail.com>
2022-08-04 00:33:52 -0400

    Finished up IAM setup for roles and policies.

--- AWS/Course_Introduction_to_AWS_AppSync-master/terraform/main.tf
+++ AWS/Course_Introduction_to_AWS_AppSync-master/terraform/main.tf
@@ -1,16 +1,40 @@
 provider "aws" {
   region = "us-east-2"
 }
+
 data "aws_iam_policy_document" "cross_account_assume_role_policy" {
   statement {
     effect = "Allow"
 
     principals {
-      type        = "AWS"
+      type        = "Service"
       identifiers = var.principal_arns
     }
 
     actions = ["sts:AssumeRole"]
+  }
+}
+
+data "aws_iam_policy_document" "db_access_policy_document" {
+  statement {
+    actions = [
+      "dynamodb:batchgetitem",
+      "dynamodb:batchwriteitem",
+      "dynamodb:putitem",
+      "dynamodb:deleteitem",
+      "dynamodb:getitem",
+      "dynamodb:scan",
+      "dynamodb:query",
+      "dynamodb:updateitem"
+    ]
+
+    resources = [
+      #"arn:aws:dynamodb:*:*:table/*/index",
+      "arn:aws:dynamodb:*:*:table/QuizBizQuestions",
+      "arn:aws:dynamodb:*:*:table/QuizBizQuestions/*"
+    ]
+
+    effect = "Allow"
   }
 }
 
@@ -23,51 +47,34 @@ resource "aws_budgets_budget" "testing-only" {
   time_period_start = "2022-04-01_00:01"
 }
 
-resource "aws_iam_policy" "ddb-access-policy" {
-  name        = "DdbAccess"
-  description = "DynamoDB Access"
-
-  policy = <<EOT
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "QuizBizQuestionTable",
-      "Effect": "Allow",
-      "Action": [
-        "dynamodb:BatchGetItem",
-        "dynamodb:BatchWriteItem",
-        "dynamodb:PutItem",
-        "dynamodb:DeleteItem",
-        "dynamodb:GetItem",
-        "dynamodb:Scan",
-        "dynamodb:Query",
-        "dynamodb:UpdateItem"
-      ],
-      "Resource": [
-        "arn:aws:dynamodb:*:*:table/QuizBizQuestions",
-        "arn:aws:dynamodb:*:*:table/QuizBizQuestions/*"
-       ]
-    }
-  ]
-}
-EOT
+resource "aws_iam_policy" "db_access_policy" {
+  name        = var.dbaccess-policy-name
+  description = "DynamoDB access to Quiz Biz Questions"
+  policy      = data.aws_iam_policy_document.db_access_policy_document.json
 }
 
-resource "aws_iam_role" "dbaccess_account" {
-  name               = var.dbaccess-name
-  assume_role_policy = ddb-access-policy
-}
-
-resource "aws_iam_role" "cross_account_assume_role" {
+resource "aws_iam_role" "logging_account_role" {
   name               = var.logging-name
   assume_role_policy = data.aws_iam_policy_document.cross_account_assume_role_policy.json
 }
 
-resource "aws_iam_role_policy_attachment" "cross_account_assume_role" {
-  count = length(var.policy_arns)
+resource "aws_iam_role" "db_access_account_role" {
+  name               = var.dbaccess-name
+  assume_role_policy = data.aws_iam_policy_document.cross_account_assume_role_policy.json
+  #assume_role_policy = data.aws_iam_policy_document.db_access_policy_document.json
+}
 
-  role       = aws_iam_role.cross_account_assume_role.name
-  policy_arn = element(var.policy_arns, count.index)
+resource "aws_iam_role_policy_attachment" "logging_role_attachment" {
+  role       = aws_iam_role.logging_account_role.name
+  policy_arn = var.logging_arn
+}
+
+resource "aws_iam_role_policy_attachment" "db_access_role_attachment" {
+  role       = aws_iam_role.db_access_account_role.name
+  policy_arn = aws_iam_policy.db_access_policy.arn
+}
+
+output "db_access_policy_document" {
+  value = data.aws_iam_policy_document.db_access_policy_document.json
 }
 
--- AWS/Course_Introduction_to_AWS_AppSync-master/terraform/variables.tf
+++ AWS/Course_Introduction_to_AWS_AppSync-master/terraform/variables.tf
@@ -8,14 +8,19 @@ variable "dbaccess-name" {
   type        = string
   description = "The name of the role."
 }
+variable "dbaccess-policy-name" {
+  default     = "QuizBizQuestionTable"
+  type        = string
+  description = "Access to Quiz Biz Questions in DynamoDB"
+}
 variable "principal_arns" {
   default     = ["appsync.amazonaws.com"]
   type        = list(string)
   description = "ARNs of accounts, groups, or users with the ability to assume this role."
 }
-variable "policy_arns" {
-  default     = ["arn:aws:iam::aws:policy/service-role/AWSAppSyncPushToCloudWatchLogs"]
-  type        = list(string)
+variable "logging_arn" {
+  default     = "arn:aws:iam::aws:policy/service-role/AWSAppSyncPushToCloudWatchLogs"
+  type        = string
   description = "List of ARNs of policies to be associated with the created IAM role."
 }
 
